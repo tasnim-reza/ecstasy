3261aa3ed315953f9547390c62b29a4ad5f43a2f
Tasnim Reza <reza5619@gmail.com>
2016-03-08 10:43:09 +0600

    updated design pattern

--- src/app.js
+++ src/app.js
@@ -1,80 +1,131 @@
-/**
- * Created by tasnim.reza on 3/6/2016.
- */
-
-var Participant = function (name) {
-    this.name = name;
-    this.chatroom = null;
-};
-
-Participant.prototype = {
-    send: function (message, to) {
-        this.chatroom.send(message, this, to);
-    },
-    receive: function (message, from) {
-        log.add(from.name + " to " + this.name + ": " + message);
-    }
-};
-
-var Chatroom = function () {
+//mediator
+function mediator() {
     var participants = {};
+    var state = {};
 
-    return {
+    this.register = function (participant) {
+        participants[participant.name] = participant;
+        participant.mediator = this;
+        participant.state = state;
+    }
 
-        register: function (participant) {
-            participants[participant.name] = participant;
-            participant.chatroom = this;
-        },
-
-        send: function (message, from, to) {
-            if (to) {                      // single message
-                to.receive(message, from);
-            } else {                       // broadcast message
-                for (key in participants) {
-                    if (participants[key] !== from) {
-                        participants[key].receive(message, from);
-                    }
+    this.dispatch = function (message, from, to) {
+        if (to)
+            to.receive(message, from);
+        else
+            for (var key in participants) {
+                if (participants[key] !== from) {
+                    participants[key].receive(message, from);
                 }
             }
+    }
+
+    this.run = function (name, event, element) {
+        if (!participants[name]) {
+            console.log('unhandled action: ', name);
+            return;
+        } else {
+            console.log('handled action: ', name);
         }
-    };
-};
 
-// log helper
+        var participant = participants[name];
 
-var log = (function () {
-    var log = "";
+        var targetType = event.target.type;
+        var targetValue = null;
+        if (targetType === 'text') {
+            targetValue = parseInt(event.target.value);
+        }
 
-    return {
-        add: function (msg) {
-            log += msg + "\n";
-        },
-        show: function () {
-            alert(log);
-            log = "";
+        participant.execute(targetValue);
+
+        //call the updaters
+        for (var t = 0; t < (participant.targets && participant.targets.length); t++) {
+            var target = participant.targets[t];
+            var targetParticipant = participants[target];
+            targetParticipant.update();
         }
     }
-})();
-
-function run() {
-    var yoko = new Participant("Yoko");
-    var john = new Participant("John");
-    var paul = new Participant("Paul");
-    var ringo = new Participant("Ringo");
-
-    var chatroom = new Chatroom();
-    chatroom.register(yoko);
-    chatroom.register(john);
-    chatroom.register(paul);
-    chatroom.register(ringo);
-
-    yoko.send("All you need is love.");
-    yoko.send("I love you John.");
-    john.send("Hey, no need to broadcast", yoko);
-    paul.send("Ha, I heard that!");
-    ringo.send("Paul, what do you think?", paul);
-
-    log.show();
 }
 
-//run();
+//producer
+function producer(med, participant) {
+    med.register(participant);
+}
+
+function consumer(med, participant) {
+    med.dispatch("Hello World", participant);
+}
+
+var participant = function (name) {
+    this.name = name;
+    this.mediator = null;
+
+    this.dispatch = function (message, to) {
+        this.mediator.dispatch(message, this, to);
+    };
+
+    this.receive = function (message, from) {
+        console.log(from.name + " to " + this.name + ": " + message);
+    }
+};
+
+function main() {
+    med = new mediator();
+
+    var incrementOnClickAction = {
+        name: 'buttonIncrement:click',
+        execute: function () {
+            if (!this.state.count) this.state.count = 0;
+            this.state.count++;
+
+            this.state.color = 'rgb(' + 10 * this.state.count + ', ' + 40 * this.state.count + ', ' + 30 * this.state.count + ');';
+        },
+        targets: ['result', 'buttonIncrement:click', 'countResult:keyup'],
+        update: function () {
+            document.getElementById('buttonIncrement').style = 'background-color: ' + this.state.color;
+        }
+    };
+    med.register(incrementOnClickAction);
+
+    var incrementOnMouseDownAction = {
+        name: 'buttonIncrement:mousedown',
+        execute: function () {
+            if (!this.state.count) this.state.count = 0;
+            this.state.count++;
+
+            this.state.color = 'rgb(' + 10 * this.state.count + ', ' + 40 * this.state.count + ', ' + 30 * this.state.count + ');';
+        },
+        targets: ['result', 'buttonIncrement:click'],
+        update: function () {
+            document.getElementById('buttonIncrement').style = 'background-color: ' + this.state.color;
+        }
+    };
+    med.register(incrementOnMouseDownAction);
+
+    var incrementView = {
+        name: 'result',
+        update: function () {
+            document.getElementById(this.name).innerText = this.state.count;
+        }
+    }
+    med.register(incrementView);
+
+    var countResultView = {
+        name: 'countResult:keyup',
+        execute: function (value) {
+            if (!this.state.count) this.state.count = 0;
+            this.state.count = value;
+        },
+        update: function () {
+            document.getElementById('countResult').value = this.state.count;
+        },
+        targets: ['result']
+    }
+    med.register(countResultView);
+}
+main();
+
+function eventHandler(event, element) {
+    var actionName = event.target.id + ':' + event.type;
+    med.run(actionName, event, element);
+}
