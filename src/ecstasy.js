330236bbc883c40e34e2d454e49ab52bac733b3c
Tasnim Reza <reza5619@gmail.com>
2016-03-14 17:56:36 +0600

    code refactor

--- src/ecstasy.js
+++ src/ecstasy.js
@@ -1,160 +1,315 @@
 /**
  * Created by Reza on 11-3-16.
  */
-(function mediator() {
-    var participants = {};
-    var initializedComponents = [];
-    var state = {};
-    var dom = {};
+(function bubbleFlow() {
+
+    var bubbleFlow = ["viewActions", "modelStateUpdaters", "elementStateUpdaters", "viewUpdaters"];
+
+    var participants = [];
+    var events = {};
+    var state = {
+        event: {
+            publish: function (eventName, event) {
+                participants.forEach(function (item) {
+                    item.publish(eventName, event);
+                })
+            }
+        }
+    };
+    var elementState = {};
+    var componentInit = {};
+    var states = {
+        state: state,
+        elementState: elementState
+    };
+
     var physicalDom = {
         'document': document,
         'body': document.body
     };
-    var events = {
+
+    var browserEvents = {
         'onclick': 'click',
         'onmousedown': 'mousedown',
         'onkeyup': 'keyup',
         'onblur': 'blur'
     };
 
-    var ecstasy = {
-        register: function (participant) {
-            participants[participant.name] = participant;
+    (function initBubbler() {
+        addEventListeners();
+        //lazy call
+        setTimeout(function () {
+            callInitMethods();
+        });
+    })();
 
-            participant.id = participant.name.split(':')[0];
-            participant.mediator = this;
-            participant.state = state;
-            participant.elementState ={
-                reset: function(id, val){
-                    getPhysicalDom(id).value = val;
-                },
-                setInnerHtml: function(id, val){
-                    getPhysicalDom(id).innerHTML = val;
-                },
-                getInnerHtml: function(id){
-                    return getPhysicalDom(id).innerHTML;
-                },
-                append: function(id, val){
-                    var li = document.createElement('li');
-                    li.innerHTML = val;
-
-                    getPhysicalDom('todolist:ol').appendChild(li);
-                },
-                remove: function(id, val){
-                    var li = document.createElement('li');
-                    li.innerHTML = val;
-
-                    getPhysicalDom('todolist:ol').removeChild(li);
-                }
-            }
-        },
-
-        run: function (name, event, element) {
-            var componentName = name.split(':')[0];
-            var eventName = name.split(':')[1] + ':' + name.split(':')[2];
-            var participant = participants[componentName];
-
-            if (!participant || !participant['updateState:' + eventName]) {
-                console.log('unhandled action: ', eventName);
-                return;
-            } else {
-                console.log('handled action: ', eventName);
-            }
-
-            var targetType = event.target.type;
-            var targetValue = null;
-            if (targetType === 'text') {
-                targetValue = parseInt(event.target.value);
-            }
-
-            var data = event.target.dataset;
-
-            initComponent(participant);
-            updateState(eventName, participant);
-            updateView(eventName, participant);
-            //participant.updateView();
-
-            //call the updaters
+    function addEventListeners() {
+        for (var key in browserEvents) {
+            physicalDom.body.addEventListener(browserEvents[key], eventHandler, true)
         }
     }
 
-    function initComponent(participant) {
-        if (participant && !(initializedComponents.indexOf(participant.name) > -1)) {
-            initializedComponents.push(participant.name);
-            participant.createState()
+    function callInitMethods() {
+        for (var key in componentInit) {
+            componentInit[key].call(states);
         }
     }
-
-    function updateState(eventName, participant) {
-        eventName = 'updateState:' + eventName;
-
-
-
-        var data = {};
-        data['dataset'] = event.target.dataset;
-        data[event.target.id.split(':')[1]] = event.target.value;
-
-
-        participant[eventName](data);
-    }
-
-    function updateView(eventName, participant) {
-        for (var t = 0; t < (participant.targets && participant.targets.length); t++) {
-            var target = participant.targets[t];
-            var targetParticipant = participants[target];
-
-            targetParticipant.update(dom);
-            render(targetParticipant);
-        }
-    }
-
-    function render(participant) {
-        if (!physicalDom[participant.id]) {
-            physicalDom[participant.id] = document.getElementById(participant.id)
-        }
-
-        var attrs = dom[participant.id];
-
-        for (var key in attrs) {
-            physicalDom[participant.id][key] = attrs[key]
-        }
-
-    }
-
-    function initMediator() {
-        for (var key in events) {
-            physicalDom.body.addEventListener(events[key], eventHandler,true)
-        }
-    }
-
-    function getPhysicalDom(selector){
-        if(!physicalDom[selector]) physicalDom[selector] = physicalDom.document.getElementById(selector);
-        return physicalDom[selector];
-    }
-    initMediator()
 
     function eventHandler(event, element) {
         var actionName = event.target.id + ':' + event.type;
-        ecstasy.run(actionName, event, element);
+        dispatch(actionName, event, element);
     }
 
-    window.ecstasy = ecstasy;
+    function dispatch(actionName, event, element) {
+        if (!events[actionName]) {
+            console.log('unhandled action: ', actionName);
+            return;
+        } else {
+            console.log('handled action: ', actionName);
+        }
+        events[actionName].call(states, event, element);
+    }
+
+
+    this.bubbler = {
+        createModelStateUpdater: function (modelUpdater) {
+            var updater = Object.create(modelUpdater.prototype),
+                key = '',
+                onEvents = {};
+
+            updater.registerFor = function (elementId) {
+                this.on = function on(eventName, callback) {
+                    key = elementId + ':' + eventName;
+                    events[key] = callback;
+                };
+                return this;
+            };
+
+            updater.on = function on(eventName, callback) {
+                onEvents[eventName] = callback;
+            }
+
+            modelUpdater.apply(updater, []);
+
+            if (typeof(updater.initModelState) === 'function') {
+                componentInit['initModelState'] = updater.initModelState;
+            }
+
+            updater.publish = function (eventName, event) {
+                if (onEvents[eventName])
+                    onEvents[eventName].call(states,event);
+            }
+
+            participants.push(updater);
+        },
+        createElementStateStateUpdater: function (elementUpdater) {
+            var updater = Object.create(elementUpdater.prototype),
+                onEvents = {};
+            updater.registerFor = function (elementId) {
+                this.on = function (eventName, callback) {
+                    events[[elementId, eventName]] = callback;
+                }
+                return this;
+            };
+
+            updater.on = function on(eventName, callback) {
+                onEvents[eventName] = callback;
+            };
+
+            elementUpdater.apply(updater, []);
+
+            if (typeof(updater.initElementState) === 'function') {
+                componentInit['initElementState'] = updater.initElementState;
+            }
+
+            updater.publish = function (eventName, event) {
+                if (onEvents[eventName])
+                    onEvents[eventName].call(states, event);
+            }
+
+            participants.push(updater);
+        },
+        createViewUpdater: function (viewUpdater) {
+            var updater = Object.create(viewUpdater.prototype),
+                onEvents = {};
+            updater.on = function on(eventName, callback) {
+                onEvents[eventName] = callback;
+            };
+            viewUpdater.apply(updater, []);
+
+            updater.publish = function (eventName, event) {
+                if (onEvents[eventName])
+                    onEvents[eventName].call(states, event);
+            }
+            participants.push(updater);
+        }
+    }
+
+    //utility methods
+    bubbler.parse = function (tpl, data) {
+        var replacedByData = tpl.replace('{item}', data.value);
+        var replacedByEventId = replacedByData.replace(/{id}/g, data.id);
+
+        return replacedByEventId;
+    }
+
+    return this;
 })();
 
+/*(function mediator() {
+ var participants = {};
+ var initializedComponents = [];
+ var state = {};
+ var dom = {};
+ var physicalDom = {
+ 'document': document,
+ 'body': document.body
+ };
+ var events = {
+ 'onclick': 'click',
+ 'onmousedown': 'mousedown',
+ 'onkeyup': 'keyup',
+ 'onblur': 'blur'
+ };
+
+ var ecstasy = {
+ register: function (participant) {
+ participants[participant.name] = participant;
+
+ participant.id = participant.name.split(':')[0];
+ participant.mediator = this;
+ participant.state = state;
+ participant.elementState ={
+ reset: function(id, val){
+ getPhysicalDom(id).value = val;
+ },
+ setInnerHtml: function(id, val){
+ getPhysicalDom(id).innerHTML = val;
+ },
+ getInnerHtml: function(id){
+ return getPhysicalDom(id).innerHTML;
+ },
+ append: function(id, val){
+ var li = document.createElement('li');
+ li.innerHTML = val;
+
+ getPhysicalDom('todolist:ol').appendChild(li);
+ },
+ remove: function(id, val){
+ var li = document.createElement('li');
+ li.innerHTML = val;
+
+ getPhysicalDom('todolist:ol').removeChild(li);
+ }
+ }
+ },
+
+ run: function (name, event, element) {
+ var componentName = name.split(':')[0];
+ var eventName = name.split(':')[1] + ':' + name.split(':')[2];
+ var participant = participants[componentName];
+
+ if (!participant || !participant['updateState:' + eventName]) {
+ console.log('unhandled action: ', eventName);
+ return;
+ } else {
+ console.log('handled action: ', eventName);
+ }
+
+ var targetType = event.target.type;
+ var targetValue = null;
+ if (targetType === 'text') {
+ targetValue = parseInt(event.target.value);
+ }
+
+ var data = event.target.dataset;
+
+ initComponent(participant);
+ updateState(eventName, participant);
+ updateView(eventName, participant);
+ //participant.updateView();
+
+ //call the updaters
+ }
+ }
+
+ function initComponent(participant) {
+ if (participant && !(initializedComponents.indexOf(participant.name) > -1)) {
+ initializedComponents.push(participant.name);
+ participant.createState()
+ }
+ }
+
+ function updateState(eventName, participant) {
+ eventName = 'updateState:' + eventName;
+
+
+
+ var data = {};
+ data['dataset'] = event.target.dataset;
+ data[event.target.id.split(':')[1]] = event.target.value;
+
+
+ participant[eventName](data);
+ }
+
+ function updateView(eventName, participant) {
+ for (var t = 0; t < (participant.targets && participant.targets.length); t++) {
+ var target = participant.targets[t];
+ var targetParticipant = participants[target];
+
+ targetParticipant.update(dom);
+ render(targetParticipant);
+ }
+ }
+
+ function render(participant) {
+ if (!physicalDom[participant.id]) {
+ physicalDom[participant.id] = document.getElementById(participant.id)
+ }
+
+ var attrs = dom[participant.id];
+
+ for (var key in attrs) {
+ physicalDom[participant.id][key] = attrs[key]
+ }
+
+ }
+
+ function initMediator() {
+ for (var key in events) {
+ physicalDom.body.addEventListener(events[key], eventHandler,true)
+ }
+ }
+
+ function getPhysicalDom(selector){
+ if(!physicalDom[selector]) physicalDom[selector] = physicalDom.document.getElementById(selector);
+ return physicalDom[selector];
+ }
+ initMediator()
+
+ function eventHandler(event, element) {
+ var actionName = event.target.id + ':' + event.type;
+ ecstasy.run(actionName, event, element);
+ }
+
+ window.ecstasy = ecstasy;
+ })();*/
+
 /*
-screen view area. if a dom is changed in unseen area
-we actually don't need to change immediately.
-multiple view area. we can prepare multiple view area.
-but we do always only three
-current and next
-if user moved to next then we preserve the old one.
-do we actually need all the manipulated dom persist in browser ? NO
+ screen view area. if a dom is changed in unseen area
+ we actually don't need to change immediately.
+ multiple view area. we can prepare multiple view area.
+ but we do always only three
+ current and next
+ if user moved to next then we preserve the old one.
+ do we actually need all the manipulated dom persist in browser ? NO
 
-this.element.createNode('a')
-.addAttribute('data-id',1)
-.addEventListener('onclcik','console.log('call')')
-.render();
+ this.element.createNode('a')
+ .addAttribute('data-id',1)
+ .addEventListener('onclcik','console.log('call')')
+ .render();
 
 
-<a data-id="1" onclick="console.log('call') />
+ <a data-id="1" onclick="console.log('call') />
  */
--- src/todo-example.js
+++ src/todo-example.js
@@ -1,168 +1,72 @@
-(function main() {
-
-    var tpl = null;
-    var todoListComponent = {
-        name: 'todolist',
-        createState: function () {
-            this.state.todoList = [
-                {id: 0, value: 'call home'},
-                {id: 1, value: 'shopping'},
-                {id: 2, value: 'talk to house owner'}];
-            this.state.todoModel = 'first value';
-        },
-        ['updateState:todoModel:blur']: function (data) {
-            this.state.todoModel = data.todoModel;
-        },
-        ['updateState:addTodo:click']: function () {
-            var item = {
-                id: this.state.todoList.length.toString(),
-                value: this.state.todoModel
-            };
-            this.state.todoList.push(item);
-            this.elementState.reset('todolist:todoModel', '');
-
-            if (!tpl) tpl = this.elementState.getInnerHtml('todolist:tpl');
-            var replacedByData = tpl.replace('{item}', item.value);
-            var replacedByEventId = replacedByData.replace(/{id}/g, item.id);
-
-            this.elementState.append('todolist:tpl', replacedByEventId);
-        },
-        ['updateState:done:click']: function (data) {
-            this.state.todoList = this.state.todoList.filter(function (item) {
-                return data.id != item.id;
-            });
-
-            this.elementState.removeChild()
-        },
-        updateView: function () {
-            if (!tpl) tpl = this.elementState.getInnerHtml('todolist:tpl');
-
-            var tplList = [];
-            if (tpl.search('{item}') > 0) {
-                this.state.todoList.forEach(function (item, id) {
-                    var replacedByData = tpl.replace('{item}', item.value);
-                    var replacedByEventId = replacedByData.replace(/{id}/g, item.id);
-
-                    tplList.push(replacedByEventId);
-
-                })
-                //dom.todoList.add('item', item);
-
-                this.elementState.setInnerHtml('todolist:tpl', tplList.join(''));
-            }
-        },
-        targets: []
-    };
-    ecstasy.register(todoListComponent);
-})();
-
-(function bubbleFlow(){
-
-    var bubbleFlow =["viewActions", "modelStateUpdaters", "elementStateUpdaters", "viewUpdaters"];
-
-    var events = {};
-    var state = {
-        event:{
-            publish: function(eventName){
-                events[eventName]();
-            }
-        }
-    }
-    var elementState = {};
-
-    this.bubbleFlow={
-        createModelStateUpdater: function(modelUpdater){
-            var updater = Object.create(modelUpdater.prototype),
-                key = '';
-            updater.registerFor = function(elementId){
-                this.on = function(eventName, callback){
-                    key = elementId + ':' + eventName;
-                    events[key] = callback;
-                }
-                return this;
-            };
-
-            updater.state = state;
-            modelUpdater.apply(updater,[]);
-            var foo = events[key];
-            foo.bind(updater);
-
-            setTimeout(function(){
-                events['addTodo:click']({});
-            })
-        },
-        createElementStateStateUpdater: function(elementUpdater){
-            var updater = Object.create(elementUpdater.prototype);
-            updater.registerFor = function(elementId){
-                this.on = function(eventName, callback){
-                    events[[elementId,eventName]] = callback;
-                }
-                return this;
-            };
-
-            updater.elementState = elementState;
-            elementUpdater.apply(updater,[]);
-        },
-        createViewUpdater: function(viewUpdater){
-            var updater = Object.create(viewUpdater.prototype);
-            viewUpdater.apply(updater,[]);
-            for(var key in updater){
-                events[key] = updater[key];
-            }
-            updater.state = state;
-            updater.elementState = elementState;
-        }
-    }
-    return this;
-})();
-
-bubbleFlow.createModelStateUpdater(function(){
-    this.initModelState= function(){
+bubbler.createModelStateUpdater(function () {
+    this.initModelState = function () {
+        this.state.defaultModel ={id: 0, value: 'dhaka'};
         this.state.todModel = '';
-        this.state.todoList = [{id:0, value: 'dhaka'}, {id:0, value: 'rajshahi'}];
+        this.state.todoList = [{id: 0, value: 'dhaka'}, {id: 0, value: 'rajshahi'}];
+        this.state.event.publish('onInitModelState');
     };
 
-    this.registerFor("addTodo").on("click", function(event){
-        var listItem = {id: this.state.todoList.length+1, value: this.state.todoModel};
+    this.registerFor("addTodo").on("click", function (event) {
+        var listItem = {id: this.state.todoList.length + 1, value: this.state.todoModel};
         this.state.todoList.push(listItem);
         this.state.event.publish('onItemAdded');
     });
 
-    this.registerFor("todoModel").on("blur", function(){
+    this.registerFor("todoModel").on("blur", function (event) {
         this.state.todoModel = event.target.value;
         this.state.event.publish('onTodoModelChange');
     });
 
-    this.registerFor("done").on('click', function(event){
-        this.state.todoList = this.state.todoList.filter(function(listItem){
+    this.registerFor("done").on('click', function (event) {
+        this.state.todoList = this.state.todoList.filter(function (listItem) {
             listItem.id !== event.target.id;
         })
-        this.state.event.publish('onTodoComplete');
+        this.state.event.publish('onTodoComplete', event);
     });
 });
 
-bubbleFlow.createElementStateStateUpdater(function(){
-    this.initElementState = function(){
+bubbler.createElementStateStateUpdater(function () {
+    this.on('onInitModelState', function (event) {
         this.elementState.listItemTpl = document.getElementById('todoListTpl').innerHTML;
-    };
+        this.elementState.addedItems =[];
+    });
 
-    this.registerFor('todoListTpl').on('onItemAdded', function (){
-        var li = document.createElement('li');
-        li.innerHTML = bubble.parse(this.elementState.listItemTpl, this.state.todoModel);
+    this.on('onItemAdded', function (event) {
+        var li = document.createElement('li'),
+            addedItem = {id: this.state.todoList.length + 1, value: this.state.todoModel};
+
+        li.innerHTML = bubbler.parse(this.elementState.listItemTpl, addedItem);
         this.elementState.listItemElement = li;
-    })
+        this.elementState.addedItems.push({id: addedItem.id, element:li});
+    });
 });
 
-bubbleFlow.createViewUpdater(function(){
-    this.onTodoComplete =function(){
-        document.getElementById("todoListContainer").appendChild(this.elementState.listItemElement);
-    };
+bubbler.createViewUpdater(function () {
+    this.on('onInitModelState', function (event) {
+        var addedItem = {id: this.state.todoList.length + 1, value: this.state.defaultModel.value};
+        document.getElementById("todoListTpl").innerHTML = bubbler.parse(this.elementState.listItemTpl, addedItem);
+        var li = document.getElementById("todoListTpl");
 
-    this.onTodoModelChange = function(){
+        this.elementState.addedItems.push({id: addedItem.id, element:li});
+    });
+
+    this.on('onTodoComplete', function (event) {
+
+        var child = this.elementState.addedItems.find(function(item){
+            return item.id.toString() === event.target.dataset.id;
+        });
+
+        this.elementState.addedItems = this.elementState.addedItems.filter(function(item){
+            return item.id.toString() !== event.target.dataset.id;
+        });
+        document.getElementById("todoListContainer").removeChild(child.element);
+    });
+
+    this.on('onTodoModelChange', function () {
         document.getElementById("lblTodoModel").textContent = this.state.todoModel;
-    };
+    });
 
-    this.onItemAdded = function(){
+    this.on('onItemAdded', function () {
         document.getElementById("todoListContainer").appendChild(this.elementState.listItemElement);
-    };
+    });
 });
